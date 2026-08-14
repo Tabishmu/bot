8660709d8b8499bd1e839ef79c27432f45040b4d
Tabishmu <111897301+Tabishmu@users.noreply.github.com>
2026-08-14 20:02:18 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@ import os, telebot, yt_dlp
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 BOT_TOKEN = "8822372631:AAHfWhldF3DG2mgLIiNbEh1g5LfLzM_-qcA"
-bot = telebot.TeleBot(8822372631:AAHfWhldF3DG2mgLIiNbEh1g5LfLzM_-qcA)
+bot = telebot.TeleBot(8822372631:AAHYi5RCR5g5tlwvId4
+Pr5f7qHYeCTqLjL4)
 
 user_urls = {}
 
