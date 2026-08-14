d17cbf6186e5df776409cd4df819ad066572afaf
Tabishmu <111897301+Tabishmu@users.noreply.github.com>
2026-08-14 20:23:06 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,8 +19,7 @@ from yt_dlp import YoutubeDL
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8822372631:AAHYi5RCR5g5tlwvId4
-Pr5f7qHYeCTqLjL4")
+BOT_TOKEN = os.getenv("BOT_TOKEN)
 
 if not BOT_TOKEN:
     raise RuntimeError("8822372631:AAHYi5RCR5g5tlwvId4
