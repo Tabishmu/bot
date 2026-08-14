e453b97ae7597e4f729b45f173a08b09bcc4229f
Tabishmu <111897301+Tabishmu@users.noreply.github.com>
2026-08-14 20:35:16 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,11 +15,11 @@ from yt_dlp import YoutubeDL
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8822372631:AAHYi5RCR5g5tlwvId4
-Pr5f7qHYeCTqLjL4")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN داخل فایل .env تنظیم نشده است.")
+    raise RuntimeError("8822372631:AAHYi5RCR5g5tlwvId4
+Pr5f7qHYeCTqLjL4")
 
 bot = telebot.TeleBot(
     BOT_TOKEN,
